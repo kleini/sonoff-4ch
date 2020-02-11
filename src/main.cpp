c935b9b625730f5bc7c6a1651f48a5116f5b6f71
Marcus Klein <himself@kleini.org>
2020-02-11 22:43:05 +0100

    test latest OTA tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #define FW_NAME "Sonoff 4CH"
-#define FW_VERSION "0.1.0"
+#define FW_VERSION "0.2.0"
 
 #include <Homie.h>
 
-void setup() {
-    Serial.begin(115200);
-    Serial << endl << endl;
+const int PIN_LED = 13;
 
-    Homie_setFirmware(FW_NAME, FW_VERSION);
-    Homie.setup();
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+
+  pinMode(PIN_LED, OUTPUT);
+
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  Homie.disableResetTrigger();
+  Homie.setLedPin(PIN_LED, LOW);
+
+  Homie.setup();
 }
 
 void loop() {
-    Homie.loop();
+  Homie.loop();
 }
