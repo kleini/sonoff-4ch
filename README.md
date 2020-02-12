61e457226f89f2a676661ac91bdf8e431cdcfc65
Marcus Klein <himself@kleini.org>
2020-02-12 15:30:46 +0100

    first full version of the software

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Homie IoT based firmware for Sonoff 4CH and 4CH Pro.
 
 The device needs to be configured in self-locking mode. Other modes are not intended and not tested but may work.
 
+## Implementation details
+
+Passed through button press from STM32 normally takes about 75ms. RF signals sometimes result in wrong button presses but those then last only for the 75ms.
+Therefore when configuring the minimal button press down time down to 70ms, sometimes the wrong relays turn on. This can not be solved on the ESP8285.
+The software therefore currently keeps the minimal button down time up to 90ms, which then avoids to turn on the wrong relays on wrongly received RF signals.
+The drawback is now, that the button on the remote needs to be pressed a little bit longer.
+
+After some more testing the wrongly triggered buttons only happen, if the RF remote is very near to the Sonoff 4CH Pro. Across larger distances the received
+RF signals are never interpreted wrong and not wrong button presses appear.
+
 ## Links
 
 [Itead Wiki](https://www.itead.cc/wiki/Sonoff_4CH_Pro)
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,43 @@
 #define FW_NAME "Sonoff 4CH"
-#define FW_VERSION "0.2.0"
+#define FW_VERSION "1.0.0"
 
 #include <Homie.h>
 
+#include "ButtonNode.hpp"
+#include "RelayNode.hpp"
+
+const int PIN_BUTTON1 = 0;
+const int PIN_RELAY3 = 4;
+const int PIN_RELAY2 = 5;
+const int PIN_BUTTON2 = 9;
+const int PIN_BUTTON3 = 10;
+const int PIN_RELAY1 = 12;
 const int PIN_LED = 13;
+const int PIN_BUTTON4 = 14;
+const int PIN_RELAY4 = 15;
+
+RelayNode relay1("relay1", PIN_RELAY1);
+RelayNode relay2("relay2", PIN_RELAY2);
+RelayNode relay3("relay3", PIN_RELAY3);
+RelayNode relay4("relay4", PIN_RELAY4);
+ButtonNode button1("button1", PIN_BUTTON1, []() {
+  relay1.toggleRelay();
+});
+ButtonNode button2("button2", PIN_BUTTON2, []() {
+  relay2.toggleRelay();
+});
+ButtonNode button3("button3", PIN_BUTTON3, []() {
+  relay3.toggleRelay();
+});
+ButtonNode button4("button4", PIN_BUTTON4, []() {
+  relay4.toggleRelay();
+});
 
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
 
+  pinMode(PIN_BUTTON1, INPUT);
   pinMode(PIN_LED, OUTPUT);
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
